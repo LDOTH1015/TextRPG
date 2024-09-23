c3638da4ecdbc8da7150fe5d17f0dafe35e25f49
OTH <112844098+LDOTH1015@users.noreply.github.com>
2024-09-23 16:04:58 +0900

    Initial commit

---
+++ TextRPG/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TextRPG
+{
+    internal class Program
+    {
+        public interface ICharacter
+        {
+            public string name { get; }
+            public int health { get; set; }
+            public int attack { get; set; }
+            public bool isDead { get; set; }
+            public void TackDamage();
+        }
+
+        public class Warrior : ICharacter
+        {
+            public string name { get; }
+            public int health { get; set; }
+            public int attack { get; set; }
+            public bool isDead { get; set; }
+            public void TackDamage()
+            {
+
+            }
+        }
+
+        public class Monster : ICharacter
+        {
+            public string name { get; }
+            public int health { get; set; }
+            public int attack { get; set; }
+            public bool isDead { get; set; }
+            public void TackDamage()
+            {
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
